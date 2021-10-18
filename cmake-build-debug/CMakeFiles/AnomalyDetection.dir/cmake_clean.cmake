71971a0dc753747fc06da90e14e82a99e02e9727
Or Drukman <or.drukman@gmail.com>
2021-10-18 20:04:15 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/AnomalyDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AnomalyDetection"
+  "AnomalyDetection.pdb"
+  "CMakeFiles/AnomalyDetection.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AnomalyDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/AnomalyDetection.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AnomalyDetection.
+# This may be replaced when dependencies are built.
